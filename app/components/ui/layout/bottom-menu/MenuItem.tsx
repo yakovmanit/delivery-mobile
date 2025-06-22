10daf397a50c659cf5e071b271573a218e96d1b0
yakiv <yakov.manitskiy@gmail.com>
2025-06-22 21:50:31 +0300

    change prettier: remove useTabs

--- app/components/ui/layout/bottom-menu/MenuItem.tsx
+++ app/components/ui/layout/bottom-menu/MenuItem.tsx
@@ -4,26 +4,26 @@ import { IMenuItem, TypeNavigate } from '@/components/ui/layout/bottom-menu/menu
 import { Feather } from '@expo/vector-icons';
 
 interface IMenuItemProps {
-	item: IMenuItem;
-	nav: TypeNavigate;
-	currentRoute: string;
+  item: IMenuItem;
+  nav: TypeNavigate;
+  currentRoute: string;
 }
 
 const MenuItem: FC<IMenuItemProps> = ({ item, nav, currentRoute }) => {
-	const isActive = currentRoute === item.path;
+  const isActive = currentRoute === item.path;
 
-	return (
-		<Pressable
-			onPress={() => nav(item.path)}
-			className="items-center w-[20%]"
-		>
-			<Feather
-				name={item.icon}
-				size={26}
-				color={isActive ? '#47AA52' : '#374151'}
-			/>
-		</Pressable>
-	);
+  return (
+    <Pressable
+      onPress={() => nav(item.path)}
+      className="items-center w-[20%]"
+    >
+      <Feather
+        name={item.icon}
+        size={26}
+        color={isActive ? '#47AA52' : '#374151'}
+      />
+    </Pressable>
+  );
 };
 
 export default MenuItem;
