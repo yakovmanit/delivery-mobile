91214d60e0800cb5481e0dfc1ce7ff18317476e2
yakiv <yakov.manitskiy@gmail.com>
2025-06-22 21:26:44 +0300

    add new objects with screen data into routes array

--- app/navigation/routes.ts
+++ app/navigation/routes.ts
@@ -1,14 +1,34 @@
 import {IRoute} from "@/navigation/navigation.types";
 import Auth from "@/components/screens/auth/Auth";
 import Home from "@/components/screens/home/Home";
+import Search from '@/components/screens/search/Search';
+import Favorites from '@/components/screens/favorites/Favorites';
+import Explorer from '@/components/screens/explorer/Explorer';
+import Profile from '@/components/screens/profile/Profile';
 
 export const routes: IRoute[] = [
+  {
+    name: 'Home',
+    component: Home,
+  },
   {
     name: 'Auth',
     component: Auth,
   },
   {
-    name: 'Home',
-    component: Home,
-  }
+    name: 'Favorites',
+    component: Favorites,
+  },
+  {
+    name: 'Search',
+    component: Search,
+  },
+  {
+    name: 'Explorer',
+    component: Explorer,
+  },
+  {
+    name: 'Profile',
+    component: Profile,
+  },
 ]
