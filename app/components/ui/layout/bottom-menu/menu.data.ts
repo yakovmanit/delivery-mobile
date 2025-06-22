1165ae31dc7fe4dde96aae54d2d38d6c3142998d
yakiv <yakov.manitskiy@gmail.com>
2025-06-22 21:54:52 +0300

    add data array for bottom menu items

---
+++ app/components/ui/layout/bottom-menu/menu.data.ts
@@ -0,0 +1,25 @@
+import { IMenuItem } from '@/components/ui/layout/bottom-menu/menu.interface';
+
+
+export const menuItems: IMenuItem[] = [
+	{
+		icon: 'home',
+		path: 'Home',
+	},
+	{
+		icon: 'heart',
+		path: 'Favorites',
+	},
+	{
+		icon: 'search',
+		path: 'Search',
+	},
+	{
+		icon: 'shopping-bag',
+		path: 'Explorer',
+	},
+	{
+		icon: 'user',
+		path: 'Profile',
+	},
+];
