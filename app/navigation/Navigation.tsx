9da416c0ffc42932d233636ed25093ce084fdf5f
yakiv <yakov.manitskiy@gmail.com>
2025-06-22 23:03:16 +0300

    add BottomMenu to Navigation

--- app/navigation/Navigation.tsx
+++ app/navigation/Navigation.tsx
@@ -1,12 +1,41 @@
-import { FC } from 'react';
-import {NavigationContainer} from "@react-navigation/native";
-import PrivateNavigator from "@/navigation/PrivateNavigator";
+import { FC, useEffect, useState } from 'react';
+import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
+import PrivateNavigator from '@/navigation/PrivateNavigator';
+import BottomMenu from '@/components/ui/layout/bottom-menu/BottomMenu';
+import { useAuth } from '@/hooks/useAuth';
 
 const Navigation: FC = () => {
+  const { user } = useAuth();
+
+  const [currentRoute, setCurrentRoute] = useState<string | undefined>(undefined);
+
+  const navRef = useNavigationContainerRef();
+
+
+  useEffect(() => {
+    setCurrentRoute(navRef.getCurrentRoute()?.name);
+
+    const listener = navRef.addListener('state', () => {
+      setCurrentRoute(navRef.getCurrentRoute()?.name);
+    });
+
+    return () => {
+      navRef.removeListener('state', listener);
+    }
+  }, []);
+
   return (
-    <NavigationContainer>
-      <PrivateNavigator />
-    </NavigationContainer>
+    <>
+      <NavigationContainer ref={navRef}>
+        <PrivateNavigator />
+      </NavigationContainer>
+
+      {
+        user && currentRoute && (
+          <BottomMenu nav={navRef.navigate} currentRoute={currentRoute} />
+        )
+      }
+    </>
   );
 };
 
