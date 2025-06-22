c77a7b47e4d1d66e44ac953c8bfdb2fdfa5164b0
yakiv <yakov.manitskiy@gmail.com>
2025-06-22 21:18:34 +0300

    add new screens

---
+++ app/components/screens/explorer/Explorer.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Text, View } from 'react-native';
+
+const Explorer: FC = () => {
+  return (
+    <View>
+      <Text>Explorer</Text>
+    </View>
+  );
+};
+
+export default Explorer;
---
+++ app/components/screens/favorites/Favorites.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Text, View } from 'react-native';
+
+const Favorites: FC = () => {
+  return (
+    <View>
+      <Text>Favorites</Text>
+    </View>
+  );
+};
+
+export default Favorites;
