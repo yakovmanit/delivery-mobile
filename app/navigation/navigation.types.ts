165645e31a27d0619541188d6efd53e17362f48c
yakiv <yakov.manitskiy@gmail.com>
2025-06-22 21:24:14 +0300

    add new names of the screens into TypeRootStackParamList

--- app/navigation/navigation.types.ts
+++ app/navigation/navigation.types.ts
@@ -3,6 +3,10 @@ import {ComponentType} from "react";
 export type TypeRootStackParamList = {
   Auth: undefined
   Home: undefined
+  Favorites: undefined
+  Search: undefined
+  Explorer: undefined
+  Profile: undefined
 }
 
 export interface IRoute {
